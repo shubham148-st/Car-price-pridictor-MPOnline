aa44d3ae38287f324268aa2b1f78428ff1e70d07
SHUBHAM PURBEY <purbeyshubham@gmail.com>
2026-07-01 22:02:14 +0530

    updated files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -49,4 +50,5 @@ def predict():
         return str(e), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
